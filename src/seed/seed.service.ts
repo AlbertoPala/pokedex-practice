240ba9c5ba8f7ebc7c84dc5a9d189bcb1f88e1c7
Albertowo <pqdstar_alberto@live.com>
2024-05-17 22:52:11 -0600

    seed file populate v3

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -14,20 +14,22 @@ export class SeedService {
     private readonly pokemonModel: Model<Pokemon>,
   ) {}
   async executeSeed() {
+    await this.pokemonModel.deleteMany({}); //delete * from pokemons
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    const insertArrayPromise = [];
+    const pokemonToInsert: { name: string; no: number }[] = [];
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
       // const pokemon = await this.pokemonModel.create({ name, no });
-      insertArrayPromise.push(this.pokemonModel.create({ name, no }));
+      // insertArrayPromise.push(this.pokemonModel.create({ name, no }));
+      pokemonToInsert.push({ name, no });
     });
-    await Promise.all(insertArrayPromise);
+    await this.pokemonModel.insertMany(pokemonToInsert);
     return 'Seed executed';
   }
 }
